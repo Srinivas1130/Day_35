30e38713ad3a2231ee2dace15c175c78e19280ec
Srinivas1130 <srinivasachar1998@gmail.com>
2022-06-24 18:58:54 +0530

    Ability to get the resultby prepared statement

---
+++ PreparedStatement.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.day35;
+
+import java.sql.ResultSet;
+
+public class PreparedStatement {
+
+	public ResultSet executeQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResultSet executeQuery1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
